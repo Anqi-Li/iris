3b5352b20bf247899dc0d470b33b09501589c9b1
anqi li <anqili@anqis-MacBook-Pro.local>
2019-08-13 19:27:01 +0200

    instead of multiprocessor, use traditional for loop instead

--- save_ver_o3_loop_temp.py
+++ save_ver_o3_loop_temp.py
@@ -53,10 +53,10 @@ def main(orbit):
     sza = 90 - sun.transform_to(altaz).alt.deg
     sza = xr.DataArray(sza, coords=(mjd,), dims=('mjd',), name='sza')
     
-    #%% load apriori 
+    #% load apriori 
     apriori = xr.open_dataset('apriori_temp.nc')
     
-    #%% clip iris data
+    #% clip iris data
     #====drop data below and above some altitudes
     top = 100e3
     bot = 60e3
@@ -67,30 +67,29 @@ def main(orbit):
     z = np.arange(bot, top, 1e3) # m
     z_top = z[-1] + 2e3
     
-    #%% 1D inversion and retrieve ozone
+    #% 1D inversion and retrieve ozone
     gA_table = np.load('gA_table.npz')['gA']
     z_table = np.load('gA_table.npz')['z']
     sza_table = np.load('gA_table.npz')['sza']
     month_table = np.load('gA_table.npz')['month'] #look up gA table instead of calculate at each mjd
+    month = num2date(mjd[0], units).month
     
     fr = 0.5 # filter fraction 
     normalize = np.pi*4 / fr
     
-    def f(i):
+    o3_a = interp1d(apriori.altgrid*1e3, apriori.o3_vmr * apriori.m, fill_value="extrapolate")(z)
+    T_a = interp1d(apriori.altgrid*1e3, apriori.T, fill_value="extrapolate")(z)
+    m_a = interp1d(apriori.altgrid*1e3, apriori.m, fill_value="extrapolate")(z)
+    
+    result = []
+    for i in range(len(day_mjd_lst)):
         print(i, 'out of mjd_lst ', len(day_mjd_lst))
         
-        o3_a = interp1d(apriori.altgrid, apriori.o3_vmr*apriori.m,
-                           fill_value="extrapolate")(z)
-        T_a = interp1d(apriori.altgrid, apriori.T,
-                         fill_value="extrapolate")(z)
-        m_a = interp1d(apriori.altgrid, apriori.m,
-                         fill_value="extrapolate")(z)
     #        gA = gfactor(0.21*m_SMR, T_SMR, z, sza.sel(mjd=day_mjd_lst[i]).item())
-        month = num2date(mjd[0], units).month
         gA = interp1d(z_table, 
-                      gA_table[:,(np.abs(month_table - month)).argmin(), 0,
-                               (np.abs(sza_table - sza.sel(mjd=day_mjd_lst[i]).item())).argmin()])(z)
-        
+                  gA_table[:,(np.abs(month_table - month)).argmin(), 0,
+                           (np.abs(sza_table - sza.sel(mjd=day_mjd_lst[i]).item())).argmin()])(z)
+    
         xa = cal_o2delta(o3_a, T_a, m_a, z, sza.sel(mjd=day_mjd_lst[i]).item(), gA) * A_o2delta
         Sa = np.diag(xa**2)
         h = tan_alt.sel(mjd=day_mjd_lst[i], pixel=pixel[l1.notnull().sel(mjd=day_mjd_lst[i])])
@@ -101,17 +100,16 @@ def main(orbit):
         x, A, Ss, Sm = linear_oem(K, Se, Sa, y, xa)
     
         #lsq fit to get ozone
-        o2delta_meas = x / A_o2delta # cm-3?
+        o2delta_meas = x / A_o2delta # cm-3
         res_lsq = least_squares(residual, o3_a, bounds=(-np.inf, np.inf), verbose=0, 
                                 args=(T_a, m_a, z, sza.sel(mjd=day_mjd_lst[i]).item(), gA, o2delta_meas))
 
-        return x, xa, np.diag(Sm), A.sum(axis=1), res_lsq.x, o3_a
+        result.append((x, xa, np.diag(Sm), A.sum(axis=1), res_lsq.x, o3_a))
     
-    with Pool(processes=4) as pool:
-        result = np.array(pool.map(f, range(len(day_mjd_lst)))) #len(day_mjd_lst)
-    
+
+    result = np.array(result)
     # organize resulting arrays and save in nc file
-    result_1d = xr.DataArray(result[:,0,:], 
+    result_1d = xr.DataArray(result[:,0,:],  
                              coords=(day_mjd_lst, z), 
                              dims=('mjd', 'z'), name='VER', attrs={'units': 'photons cm-3 s-1'})
     ds = xr.Dataset({'ver': result_1d, 
@@ -123,7 +121,8 @@ def main(orbit):
     return ds
     
 if __name__ == '__main__':
-    for orbit in range(37587, 37587+50, 20):
+    path = '/home/anqil/Documents/osiris_database/iris_ver_o3/'
+    for orbit in range(37588, 37588+50, 20):
+        print('orbit: ', orbit)
         ds = main(orbit)
-        path = '/home/anqil/Documents/osiris_database/iris_ver_o3/'
         ds.to_netcdf(path+'ver_o3_{}.nc'.format(orbit))
